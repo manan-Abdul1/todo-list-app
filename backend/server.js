a307fb29166332d9f06b9f3aeb1157253fd689e1
Abdul Manan <manansheikh117@gmail.com>
2023-05-31 02:15:43 +0500

    Deploying App

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-const path = require('path');
 
 
 const app = express();
@@ -11,15 +10,7 @@ app.use(cors());
 
 const dbconfig = require('./db');
 const taskRoutes = require('./routes/taskRoutes');
-app.use(express.static(path.join(__dirname, "../frontend/build")));
-app.get("*", function (_, res) {
-  res.sendFile(
-    path.join(__dirname, "../frontend/build/index.html"),
-    function (err) {
-      res.status(500).send(err);
-    }
-  );
-});
+
 
 app.use('/', taskRoutes);
 app.listen(port, ()=> console.log("server started",port))
